4bf01bdf47c81c77d95f5872d6003468ac98cdea
G Krupa <github@bluekelp.com>
2013-04-14 16:26:44 -0700

    banner and prompt

--- uforth.asm
+++ uforth.asm
@@ -1,17 +1,30 @@
 section .data
-    s: db 'Hello, world!', 10
-	slen: equ $-s
+    banner: db 'uforth v0.0.1', 10
+	banner_len: equ $-banner
+
+    prompt_str: db 'ok', 10
+    prompt_len: equ $-prompt_str
 
 section .text
     global _start
+
+prompt:
+    mov eax,4
+    mov ebx,1
+    mov ecx,prompt_str
+    mov edx,prompt_len
+    int 80h
+    ret
 
 _start:
 	; write string to fd 1 (stdout)
     mov eax,4
     mov ebx,1
-    mov ecx,s
-    mov edx,slen
+    mov ecx,banner
+    mov edx,banner_len
     int 80h
+
+    call prompt
 
 	; exit w/0
     mov eax,1
